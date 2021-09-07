ad492f909471cd3f65e2e6bb7eee99739cbe8335
trainb0y1 <66213737+trainb0y1@users.noreply.github.com>
2021-09-07 16:22:32 -0600

    SmoothDamp() the player

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,12 +2,15 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public Transform target;
+    public Transform player;
     public Vector2 offset;
+    public float smoothTime = 0.3f;
+    private Vector3 velocity = Vector3.zero;
 
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = new Vector3(target.position.x + offset.x, target.position.y + offset.y, transform.position.z);
+        Vector3 desiredPosition = new Vector3(player.position.x + offset.x, player.position.y + offset.y, transform.position.z);
+        transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothTime);
     }
 }
